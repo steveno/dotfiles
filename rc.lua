d31213e3633baf559cefc1e26db2e72e678c215d
Steven Oliver <oliver.steven@gmail.com>
2011-09-13 23:25:19 -0400

    Start awesome config file

---
+++ rc.lua
@@ -0,0 +1,13 @@
+-- {{{ License
+--
+-- Steven Oliver's awesome configuration file
+--
+-- }}}
+
+-- {{{ Libraries
+require ("awful")
+-- }}}
+
+-- Beautiful theme
+beautiful.init(home .. "/.config/awesome/zenburn.lua")
+
