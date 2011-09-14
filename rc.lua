a1def28a7720c4d4bcf7dabdbc299938af621af3
Steven Oliver <oliver.steven@gmail.com>
2011-09-14 11:10:28 -0300

    Setup typical defaults

--- rc.lua
+++ rc.lua
@@ -17,3 +17,14 @@ require("naughty")
 -- Beautiful theme
 beautiful.init("/home/users/steveno/.config/awesome/zenburn.lua")
 
+-- This is used later as the default terminal and editor to run.
+terminal = "xterm"
+editor = "gvim"
+editor_cmd = editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+modkey = "Mod4"
