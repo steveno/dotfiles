912c84dd81e768f0b8a81586489bc13e5f600a96
Steven Oliver <oliver.steven@gmail.com>
2019-09-03 11:28:01 -0400

    Update dev scripts

--- files/home/Projects/dlang/start_d.sh
+++ files/home/Projects/dlang/start_d.sh
@@ -1,4 +1,10 @@
 # Managed by salt
 #!/bin/bash
 
+if [ "$0" = "$BASH_SOURCE" ]
+then
+    echo "Script must be sourced!"
+    exit 0;
+fi
+
 source $(~/dlang/install.sh dmd -a)
--- files/home/Projects/python/update_devsalt.sh
+++ files/home/Projects/python/update_devsalt.sh
@@ -7,10 +7,27 @@ then
     exit 0;
 fi
 
+function start_salt {
+    ulimit -n 2047
+    cd /home/steveno/Projects/python/devsalt
+    salt-master -c ./etc/salt -d
+    salt-minion -c ./etc/salt -d
+    sleep 3
+    salt -c ./etc/salt '*' test.ping
+}
+
+function kill_salt {
+    deactivate
+    pkill -f ./etc/salt
+}
+
 O_PWD=$(pwd)
 
+kill_salt
 cd /home/steveno/Projects/python/devsalt
+virtualenv --clear .
 source bin/activate
-pip install --upgrade -e ../salt/
+pip install -e ../salt/
+start_salt
 
 cd $O_PWD
