c028700a2ec249133c9bcc1d695c76ca1d024444
steveno <oliver.steven@gmail.com>
2020-08-28 14:24:04 -0400

    Add yubikey

--- setup.sh
+++ setup.sh
@@ -15,6 +15,9 @@ wget https://pkg.tarsnap.com/tarsnap-deb-packaging-key.asc
 sudo apt-key add tarsnap-deb-packaging-key.asc
 echo "deb http://pkg.tarsnap.com/deb/$(lsb_release -s -c) ./" | sudo tee -a /etc/apt/sources.list.d/tarsnap.list
 
+# yubikey
+sudo add-apt-repository ppa:yubico/stable
+
 # Update apt sources
 sudo apt-get update
 
@@ -28,6 +31,9 @@ sudo apt install calibre exfat-fuse fonts-ibm-plex keepassxc libreoffice \
 
 # Install vala specific packages
 sudo apt install libgtk-3-dev libgee-0.8-dev libsqlite3-dev valac
+
+# Install yubikey packages
+sudo apt install yubikey-manager yubioath-desktop yubikey-personalization-gui
 
 # gnupg
 mkdir ~/.gnupg
