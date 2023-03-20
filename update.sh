bd6996b77e8791a76bccee7b88e40dce03a015c4
steveno <oliver.steven@gmail.com>
2023-03-19 20:53:01 -0400

    Modify update script to only run locally

--- update.sh
+++ update.sh
@@ -8,11 +8,11 @@ set -o errexit
 set -o pipefail
 
 # Update tarsnap script and configuration
-sudo cp ~/Projects/dotfiles/tarsnap/tarsnap.conf /etc/
-sudo cp ~/Projects/dotfiles/tarsnap/tarsnap-backup.sh /root/
+sudo cp tarsnap/tarsnap.conf /etc/
+sudo cp tarsnap/tarsnap-backup.sh /root/
 
 # Update the tarsnap timer and service
-sudo cp ~/Projects/dotfiles/tarsnap/tarsnap.timer /etc/systemd/system/
-sudo cp ~/Projects/dotfiles/tarsnap/tarsnap.service /etc/systemd/system/
+sudo cp tarsnap/tarsnap.timer /etc/systemd/system/
+sudo cp tarsnap/tarsnap.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
