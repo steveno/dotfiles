86b59daf3edaa8d007b9434aa4348fd2a308c0e3
steveno <oliver.steven@gmail.com>
2024-03-13 22:38:05 -0400

    neovim: remap localleader to comma

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- Any copyright is dedicated to the Public Domain.
 -- https://creativecommons.org/publicdomain/zero/1.0/
 
+vim.g.maplocalleader = ","
 require('plugins')
 require('settings')
 require('autocmd')
