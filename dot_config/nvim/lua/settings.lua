0732f152a43c0e661012d514d55c4997a88016ba
steveno <oliver.steven@gmail.com>
2024-05-16 14:36:26 -0400

    Apparently nvim ignores t_* commands

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -62,12 +62,6 @@ end
 
 -- color scheme
 vim.opt.termguicolors = true
-vim.cmd([[
-    let &t_8f =  "\<Esc>[38:2;%lu;%lu;%lum"
-    let &t_8b =  "\<Esc>[48:2;%lu;%lu;%lum"
-    set t_Co=256
-]])
-
 vim.cmd.colorscheme('paper')
 
 -- Python
