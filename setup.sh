c1a7748d15e37d8b0b07f8a68a7744fbc58d0d8c
steveno <oliver.steven@gmail.com>
2024-07-22 00:07:28 -0400

    Remove yubikey setup

--- setup.sh
+++ setup.sh
@@ -7,23 +7,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-# yubikey
-sudo add-apt-repository ppa:yubico/stable
-
-sudo apt update
-
-# Install yubikey packages
-sudo apt install yubikey-manager yubioath-desktop \
-    yubikey-personalization-gui
-
 # Install development related packages
 sudo apt install build-essential cmake curl meson python3 \
     python3-pip
 
-# Install general purpose packages
-sudo apt install calibre exfat-fuse fonts-ibm-plex keepassxc \
-    libreoffice mtp-tools simple-scan tarsnap texstudio vlc
-
 # Install vala specific packages
 sudo apt install libgtk-3-dev libgee-0.8-dev libsqlite3-dev valac
+
+# Install general purpose packages
+sudo apt install calibre exfat-fuse fonts-ibm-plex \
+    libreoffice mtp-tools simple-scan tarsnap texstudio vlc
 
