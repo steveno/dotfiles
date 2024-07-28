f544951ff32bd3a7434b04812146d3e6cdb7acec
steveno <oliver.steven@gmail.com>
2024-07-27 23:01:33 -0400

    Add hack to package to install

--- setup.sh
+++ setup.sh
@@ -12,9 +12,10 @@ sudo apt install build-essential cmake curl meson python3 \
     python3-pip
 
 # Install vala specific packages
-sudo apt install libgtk-3-dev libgee-0.8-dev libsqlite3-dev valac
+sudo apt install libgtk-3-dev libgee-0.8-dev libsqlite3-dev \
+    valac
 
 # Install general purpose packages
 sudo apt install calibre exfat-fuse fonts-ibm-plex \
-    libreoffice mtp-tools simple-scan tarsnap texstudio vlc
-
+    libreoffice mtp-tools simple-scan tarsnap texstudio \
+    fonts-hack-ttf vlc
