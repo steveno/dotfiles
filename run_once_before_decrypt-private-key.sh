2959dd742dfecce4285d9bc1d3a5683a68f89ff5
steveno <oliver.steven@gmail.com>
2024-09-02 01:00:27 -0400

    Add license header to script

--- run_once_before_decrypt-private-key.sh
+++ run_once_before_decrypt-private-key.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Any copyright is dedicated to the Public Domain.
+# https://creativecommons.org/publicdomain/zero/1.0/
+
 if [ ! -f "/home/steveno/.config/chezmoi/key.txt" ]; then
     mkdir -p "/home/steveno/.config/chezmoi"
     chezmoi age decrypt --output "/home/steveno/.config/chezmoi/key.txt" --passphrase "{{ .chezmoi.sourceDir }}/key.txt.age"
