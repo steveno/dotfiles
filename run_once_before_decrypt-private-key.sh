9489d50d396883543fe9ca44eecd5a18fbf3f0b0
Steven Oliver <oliver.steven@gmail.com>
2024-09-04 15:28:55 -0400

    Update run_once_before_decrypt-private-key.sh
    Add my standard bash sets at the top

--- run_once_before_decrypt-private-key.sh
+++ run_once_before_decrypt-private-key.sh
@@ -3,6 +3,10 @@
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 if [ ! -f "/home/steveno/.config/chezmoi/key.txt" ]; then
     mkdir -p "/home/steveno/.config/chezmoi"
     chezmoi age decrypt --output "/home/steveno/.config/chezmoi/key.txt" --passphrase "{{ .chezmoi.sourceDir }}/key.txt.age"
